eef850b689c1984763f4acc8dd708783d340a0b6
Sam Parkinson <parkinsonsp42@gmail.com>
2019-03-03 21:54:45 -0500

    updated style

---
+++ index.js
@@ -0,0 +1,16 @@
+function sectionClicked() {
+  // check if the screen is a certain size
+  // check if the section name was clicked
+  // call relevant function
+}
+
+function showSection() {
+  // show clicked section
+  // hide all other sections
+}
+
+function goToSection() {
+  // go to clicked section
+}
+
+$(sectionClicked);
