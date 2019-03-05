d2709158c07cb111ff34aea85d6da185f0818b3b
Sam Parkinson <parkinsonsp42@gmail.com>
2019-03-05 00:15:15 -0500

    updated js functionality

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 function sectionClicked() {
   // check if the screen is a certain size
+  $('.navbar li, h1').on('click', event => {
+    const target = $(event.currentTarget).text().toLowerCase();
+    if (target === 'sam parkinson') {
+      showSection('.default');
+    } else {
+      showSection('.' + target);
+    }
+  })
   // check if the section name was clicked
   // call relevant function
 }
 
-function showSection() {
-  // show clicked section
-  // hide all other sections
+function showSection(clicked) {
+  $('.default, .portfolio, .about, .contact').addClass('hidden')
+  $(clicked).removeClass('hidden');
 }
 
 function goToSection() {
   // go to clicked section
 }
 
-$(sectionClicked);
+function screenExpand() {
+  // the screen expands past x pixels
+}
+
+function screenContract() {
+  // the screen shrinks 
+}
+
+function pageLoaded() {
+  sectionClicked();
+  screenExpand();
+  screenContract();
+}
+
+$(pageLoaded);
